9f5102694df05b6ad535b2d3cb8554f29e34c37f
SallyMulupi <mulupisally2gmail.com>
2022-06-22 01:43:02 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function displayName() {
+    let input = document.querySelector('#input').value;
+    let name = document.querySelector('p');
+    name.innerHTML = "Hello, " + input + " welcome to the bootcamp "
+
+
+}
